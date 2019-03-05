45f157b9c23d05687476a6eee40a21d2d69f6f5c
gmprii <gmprii@gmail.com>
2019-03-05 07:15:15 +0100

    config changes

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,9 +1,6 @@
 set :user, "danbooru"
 set :rails_env, "production"
-server "kagamihara", :roles => %w(web app db), :primary => true
-server "shima", :roles => %w(web app)
-server "saitou", :roles => %w(web app)
-server "oogaki", :roles => %w(worker)
+server "fillybooru", :roles => %w(web app db), :primary => true
 
 set :linked_files, fetch(:linked_files, []).push(".env.production")
 set :rbenv_path, "/home/danbooru/.rbenv"
